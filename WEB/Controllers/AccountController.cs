b86d3227b3e348c9cfa63415fa6b2377ea83ee86
hrshuv0 <h.r.shuvo9@gmail.com>
2023-08-19 23:17:28 +0600

    add login/ register design

---
+++ WEB/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure.Core.Entities.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WEB.Controllers;
+
+public class AccountController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+    
+    [HttpGet]
+    public IActionResult Register()
+    {
+        var model = new RegisterVm();
+        
+        return View(model);
+    }
+}
