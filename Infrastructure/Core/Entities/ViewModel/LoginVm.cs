547c6758624305de90a266e175056a376f00ecf5
hrshuv0 <h.r.shuvo9@gmail.com>
2023-08-19 22:53:04 +0600

    add login view model

---
+++ Infrastructure/Core/Entities/ViewModel/LoginVm.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Core.Entities.ViewModel;
+
+public class LoginVm
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    [Required]
+    public string Password { get; set; }
+    
+    public bool RememberMe { get; set; }
+}
---
+++ Infrastructure/Core/Entities/ViewModel/RegisterVm.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Core.Entities.ViewModel;
+
+public class RegisterVm
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    [Required]
+    public string Password { get; set; }
+
+    [Compare("Password", ErrorMessage = "Passwords do not match")]
+    public string ConfirmPassword { get; set; }
+
+    public string? Name { get; set; }
+}
