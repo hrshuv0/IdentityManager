48f7dbcb1f18b41e4927278a5ceb9cbdf012d52f
hrshuv0 <h.r.shuvo9@gmail.com>
2023-08-20 22:41:54 +0600

    Register and logout

--- WEB/Controllers/AccountController.cs
+++ WEB/Controllers/AccountController.cs
@@ -1,10 +1,21 @@
-﻿using Infrastructure.Core.Entities.ViewModel;
+﻿using Infrastructure.Core.Entities;
+using Infrastructure.Core.Entities.ViewModel;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WEB.Controllers;
 
 public class AccountController : Controller
 {
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
     // GET
     public IActionResult Index()
     {
@@ -23,5 +34,54 @@ public class AccountController : Controller
         var model = new RegisterVm();
         
         return View(model);
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Register(RegisterVm model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        var user = new ApplicationUser()
+        {
+            UserName = model.Email,
+            Email = model.Email,
+            Name = model.Name
+        };
+        
+        var result = await _userManager.CreateAsync(user, model.Password);
+
+        if (result.Succeeded)
+        {
+            await _signInManager.SignInAsync(user, false);
+        }
+        else
+        {
+            AddErrors(result);
+            return View(model);
+        }
+        
+        return RedirectToAction("Index", "Home");
+        
+    }
+    
+    private void AddErrors(IdentityResult result)
+    {
+        foreach (var error in result.Errors)
+        {
+            ModelState.AddModelError("", error.Description);
+        }
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> LogOff()
+    {
+        await _signInManager.SignOutAsync();
+        
+        return RedirectToAction("Index", "Home");
     }
 }
