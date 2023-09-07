da3e225ff8854acc1f95224050519df188947cce
hrshuv0 <h.r.shuvo9@gmail.com>
2023-09-07 14:10:22 +0600

    setup for authorization

---
+++ WEB/Controllers/AccessCheckerController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB.Controllers;
+
+public class AccessCheckerController : Controller
+{
+    // GET
+    public IActionResult AllAccess()
+    {
+        return View();
+    }
+    
+    public IActionResult AuthorizedAccess()
+    {
+        return View();
+    }
+    
+    public IActionResult UserAccess()
+    {
+        return View();
+    }
+    
+    public IActionResult AdminAccess()
+    {
+        return View();
+    }
+    
+    public IActionResult Admin_CreateAccess()
+    {
+        return View();
+    }
+    
+    public IActionResult Admin_Create_Edit_Delete()
+    {
+        return View();
+    }
+    
+    public IActionResult Admin_Create_Edit_Delete_SuperAdmin()
+    {
+        return View();
+    }
+    
+    
+    
+}
